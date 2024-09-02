c5efd95d910c4f44b03122f6ca05e20b03c4e17d
hey-ak <akshay0127.be21@chitkara.edu.in>
2024-09-02 17:02:05 +0530

    Added Swiftui Profile Project

---
+++ profileApp01/profileApp01App.swift
@@ -0,0 +1,17 @@
+//
+//  profileApp01App.swift
+//  profileApp01
+//
+//  Created by Akshay Jha on 02/09/24.
+//
+
+import SwiftUI
+
+@main
+struct profileApp01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
